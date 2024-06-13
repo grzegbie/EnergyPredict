2315567b33495b33a1c83499b590a3688cf86ebb
grzegbie <grzegbiel@gmail.com>
2024-06-13 18:04:42 +0200

    Turned off solar_radiation values other than direct

--- config.py
+++ config.py
@@ -21,16 +21,15 @@ class Config:
     HISTORICAL_ENCODED_CATEGORICAL_FEATURES = ['product_type']
 
     FORECAST_USED_FEATURES = ['latitude', 'longitude', 'temperature', 'cloudcover_total', 'direct_solar_radiation',
-                              'surface_solar_radiation_downwards', 'is_business_0', 'is_business_1', 'product_type_0',
-                              'product_type_1', 'product_type_2', 'product_type_3']
+                              'is_business_0', 'is_business_1', 'product_type_0', 'product_type_1', 'product_type_2',
+                              'product_type_3']
 
     HISTORICAL_BUSINESS_USED_FEATURES = ['latitude', 'longitude', 'temperature', 'cloudcover_total',
-                                         'shortwave_radiation', 'direct_solar_radiation', 'diffuse_radiation',
-                                         'product_type_0', 'product_type_1', 'product_type_2', 'product_type_3']
+                                         'direct_solar_radiation', 'product_type_0', 'product_type_1', 'product_type_2',
+                                         'product_type_3']
 
     HISTORICAL_PRIVATE_USED_FEATURES = ['latitude', 'longitude', 'temperature', 'cloudcover_total',
-                                        'shortwave_radiation', 'direct_solar_radiation', 'diffuse_radiation',
-                                        'product_type_1', 'product_type_2', 'product_type_3']
+                                        'direct_solar_radiation', 'product_type_1', 'product_type_2', 'product_type_3']
 
     USED_TARGET = ['target']
 
